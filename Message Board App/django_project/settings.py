1e1a4e3ee5bb01f197bae19f37b81f0350e74dad
Spartan-119 <abinvarghese90@gmail.com>
2024-08-25 19:31:28 +0100

    done till pg 91

--- Message Board App/django_project/settings.py
+++ Message Board App/django_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@&iu6a+ng87f=wa$vn*7o!)s*%fmdu!_^@o(ljo49*27$#iym'
+SECRET_KEY = 'django-insecure-c(_p1hij6$6!n)g#c%yms_dylyqg+o#t6o++3-5_yrb*+4!ni7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
